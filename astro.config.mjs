884f0a22426e26913044907ab104565021644393
Justin Gery <jaygery90@gmail.com>
2026-02-05 14:06:35 -0600

    added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://wordhopeinternational.org",
+
+  integrations: [
+    react(),
+    sitemap(),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
